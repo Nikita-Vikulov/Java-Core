cbd8aa12349276544f4a0f468d97dfecccb92aa5
Nikita-Vikulov <75396341+Nikita-Vikulov@users.noreply.github.com>
2021-02-17 21:26:19 +0300

    HomeWork4

---
+++ HomeWork8/src/main/java/server/ClientHandler.java
@@ -0,0 +1,145 @@
+package server;
+
+import com.google.gson.Gson;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ClientHandler {
+
+    private Socket socket;
+    private MyServer myServer;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private String nick;
+
+    public ClientHandler(MyServer myServer, Socket socket) {
+        try {
+            this.myServer = myServer;
+            this.socket = socket;
+            this.dataInputStream = new DataInputStream(socket.getInputStream());
+            this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            ExecutorService executorService = Executors.newFixedThreadPool(3);
+            executorService.execute(new Runnable() {
+                public void run() {
+                    try {
+                        authentication();
+                        readMessages();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } finally {
+                        closeConnection();
+                    }
+                }
+            });
+            executorService.shutdown();
+/*
+            new Thread(() -> {
+                try {
+                    authentication();
+                    readMessages();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    closeConnection();
+                }
+            }).start();*/
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeConnection() {
+        myServer.unsubscribe(this);
+        Message message = new Message();
+        message.setMessage(nick + " вышел из чата");
+        myServer.broadcastMessage(message);
+        try {
+            dataOutputStream.close();
+            dataInputStream.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void authentication() {
+        while (true) {
+            try {
+                AuthMessage message = new Gson().fromJson(dataInputStream.readUTF(), AuthMessage.class);
+                String nick = myServer.getAuthService().getNickByLoginAndPass(message.getLogin(), message.getPassword());
+                if (nick != null && !myServer.isNickBusy(nick)) {
+                    message.setAuthenticated(true);
+                    message.setNick(nick);
+                    this.nick = nick;
+                    dataOutputStream.writeUTF(new Gson().toJson(message));
+                    Message broadcastMsg = new Message();
+                    broadcastMsg.setMessage(nick + " вошел в чат");
+                    myServer.broadcastMessage(broadcastMsg);
+                    myServer.subscribe(this);
+                    this.nick = nick;
+                    return;
+                }
+            } catch (IOException ignored) {
+            }
+        }
+    }
+
+    private void readMessages() throws IOException {
+        while (true) {
+            Message message = new Gson().fromJson(dataInputStream.readUTF(), Message.class);
+            message.setNick(nick);
+            System.out.println(message);
+            if (!message.getMessage().startsWith("/")) {
+                myServer.broadcastMessage(message);
+                continue;
+            }
+            // /<command> <message>
+            String[] tokens = message.getMessage().split("\\s");
+            switch (tokens[0]) {
+                case "/end": {
+                    return;
+                }
+                case "/w": {// /w <nick> <message>
+                    if (tokens.length < 3) {
+                        Message msg = new Message();
+                        msg.setMessage("Не хватает параметров, необходимо отправить команду следующего вида: /w <ник> <сообщение>");
+                        this.sendMessage(msg);
+                    }
+                    String nick = tokens[1];
+                    String msg = tokens[2];
+                    myServer.sendMsgToClient(this, nick, msg);
+                    break;
+                }
+                case "/r": {// /r <oldNick> <newNick>
+                    if (tokens.length < 3) {
+                        Message msg = new Message();
+                        msg.setMessage("Не хватает параметров, необходимо отправить команду следующего вида: /r <старый_ник> <новый_ник> ");
+                        this.sendMessage(msg);
+                    }
+                    String oldNick = tokens[1];
+                    String newNick = tokens[2];
+                    BaseAuthService.renameUserNick(oldNick, newNick);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void sendMessage(Message message) {
+        try {
+            dataOutputStream.writeUTF(new Gson().toJson(message));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNick() {
+        return nick;
+    }
+}
